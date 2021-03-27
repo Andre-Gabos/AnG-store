e98e4549da65905415d94cf8437e11ae024ae95d
André Gabos <andre.gabos@gmail.com>
2021-03-27 17:22:14 -0300

    Adição do componente Header, icone e links.

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import {Route, Switch} from "react-router-dom";
 import './App.css';
 import Homepage from "./Pages/Homepage/Homepage";
 import Shop from "./Pages/Shop/Shop";
+import Header from "./Components/Header/Header";
 
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={Homepage} />
         <Route path="/shop" component={Shop} />
