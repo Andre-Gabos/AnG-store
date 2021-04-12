dc8ad19d1fae8e4cd35ca78cf20f34467fc869ff
André Gabos <andre.gabos@gmail.com>
2021-04-12 14:38:43 -0300

    Mudanças no dropdown para esconê-lo assim que página de checkout for aberta.

--- src/Components/CartDropdown/CartDropdown.jsx
+++ src/Components/CartDropdown/CartDropdown.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { connect } from "react-redux";
 import { selectCartItems } from "../../Redux/Cart/cart.selectors";
+import { toggleCartHidden } from "../../Redux/Cart/cart.actions";
 import { createStructuredSelector } from "reselect";
 import { withRouter } from "react-router-dom";
 import CustomButton from "../CustomButton/CustomButton";
 import CartItem from "../CartItem/CartItem";
 import "./CartDropdown.styles.scss";
 
-function CartDropdown({ cartItems, history }) {
+function CartDropdown({ cartItems, history, dispatch }) {
   return (
     <div className="cart-dropdown">
       <div className="cart-items" >
@@ -25,7 +26,12 @@ function CartDropdown({ cartItems, history }) {
             <span className="empty-message" >Your cart is empty</span>
         }
       </div>
-      <CustomButton onClick={() => history.push("/checkout")} >GO TO CHECKOUT</CustomButton>
+      <CustomButton
+        onClick={() => {
+          history.push("/checkout");
+          dispatch(toggleCartHidden());
+        }}
+      >GO TO CHECKOUT</CustomButton>
     </div>
   )
 }
