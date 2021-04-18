b37a9a384c521a2863cb6ed6074c7719f86738df
André Gabos <andre.gabos@gmail.com>
2021-04-18 01:10:18 -0300

    Inclusão do conteúdo do componente Collection, alterações de estilo e normalização dos dados.

--- src/Pages/Collection/Collection.jsx
+++ src/Pages/Collection/Collection.jsx
@@ -5,9 +5,20 @@ import CollectionItem from "../../Components/CollectionItem/CollectionItem";
 import "./Collection.styles.scss";
 
 function Collection({ collection }) {
+  const { title, items } = collection;
   return (
     <div className="collection-page">
-      <h2>COLLECTION PAGE</h2>
+      <h2 className="title">{title}</h2>
+      <div className="items">
+        {items.map((item) => {
+          return (
+            <CollectionItem 
+              key={item.id}
+              item={item}
+            />
+          )
+        })}
+      </div>
     </div>
   )
 }
