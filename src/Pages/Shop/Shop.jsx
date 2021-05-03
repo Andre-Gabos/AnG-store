4697cdd656823eae799b34e36fb974eebfa0561d
André Gabos <andre.gabos@gmail.com>
2021-05-03 17:51:07 -0300

    Correção de bug

--- src/Pages/Shop/Shop.jsx
+++ src/Pages/Shop/Shop.jsx
@@ -2,15 +2,30 @@ import React from "react";
 import { Route } from 'react-router-dom';
 import CollectionOverview from "../../Components/CollectionOverview/CollectionOverview";
 import Collection from "../Collection/Collection";
+import { firestore, convertCollectionsSnapshotToMap } from "../../Firebase/firebase.utils";
 
 
-function Shop({ match }) {
-  return (
-    <div className="shop-page">
-      <Route exact path={`${match.path}`} component={CollectionOverview} />
-      <Route path={`${match.path}/:collectionId`} component={Collection} />
-    </div>
-  );
+class Shop extends React.Component {
+  unsubscribeFromSnapshot = null;
+
+  componentDidMount() {
+    const collectionRef = firestore.collection("collections");
+
+    collectionRef.onSnapshot(async snapshot => {
+      convertCollectionsSnapshotToMap(snapshot);
+    })
+  }
+
+  render() {
+    const { match } = this.props;
+    return (
+      <div className="shop-page">
+        <Route exact path={`${match.path}`} component={CollectionOverview} />
+        <Route path={`${match.path}/:collectionId`} component={Collection} />
+      </div>
+    )
+  }
+
 };
 
 
