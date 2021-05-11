4d0946ebe7b07e072fb101c19f90f681293653e5
André Gabos <andre.gabos@gmail.com>
2021-05-11 19:07:59 -0300

    Funcionalidade de criar conta e auto login.

--- src/Components/SignUp/SignUp.jsx
+++ src/Components/SignUp/SignUp.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
+import connect from "react-redux";
 import FormInput from "../FormInput/FormInput";
 import CustomButton from "../CustomButton/CustomButton";
-import {auth, createUserProfileDocument} from "../../Firebase/firebase.utils";
+import { signUpStart } from "../../Redux/User/user.actions";
 import "./SignUp.styles.scss";
 
-function SignUp() {
+function SignUp(props) {
   const [userInfo, setUserInfo] = useState({
     displayName: "",
     email: "",
@@ -12,31 +13,19 @@ function SignUp() {
     confirmPassword: ""
   });
 
-  const {displayName, email, password, confirmPassword} = userInfo;
+  const { displayName, email, password, confirmPassword } = userInfo;
 
   async function handleSubmit(event) {
     event.preventDefault();
+
+    const { signUpStart } = props;
 
     if (password !== confirmPassword) {
       alert("Passwords don't match.");
       return;
     }
 
-    try {
-      const {user} = await auth.createUserWithEmailAndPassword(email, password);
-
-      await createUserProfileDocument(user, {displayName});
-
-      setUserInfo({
-        displayName: "",
-        email: "",
-        password: "",
-        confirmPassword: ""
-      })
-      
-    } catch (error) {
-      console.error(error);
-    }
+    signUpStart({ displayName, email, password });
   }
 
   function handleChange(event) {
@@ -56,36 +45,36 @@ function SignUp() {
       <span>Sign up with your email and password</span>
       <form className="sign-up-form" onSubmit={handleSubmit}>
         <FormInput
-        type="text"
-        name="displayName"
-        value={displayName}
-        onChange={handleChange}
-        label="Display Name"
-        required
+          type="text"
+          name="displayName"
+          value={displayName}
+          onChange={handleChange}
+          label="Display Name"
+          required
         />
         <FormInput
-        type="email"
-        name="email"
-        value={email}
-        onChange={handleChange}
-        label="Email"
-        required
+          type="email"
+          name="email"
+          value={email}
+          onChange={handleChange}
+          label="Email"
+          required
         />
         <FormInput
-        type="password"
-        name="password"
-        value={password}
-        onChange={handleChange}
-        label="Password"
-        required
+          type="password"
+          name="password"
+          value={password}
+          onChange={handleChange}
+          label="Password"
+          required
         />
         <FormInput
-        type="password"
-        name="confirmPassword"
-        value={confirmPassword}
-        onChange={handleChange}
-        label="Confirm Password"
-        required
+          type="password"
+          name="confirmPassword"
+          value={confirmPassword}
+          onChange={handleChange}
+          label="Confirm Password"
+          required
         />
         <CustomButton type="submit">SIGN UP</CustomButton>
       </form>
@@ -93,4 +82,8 @@ function SignUp() {
   )
 }
 
-export default SignUp;
+const mapDispatchToProps = dispatch => ({
+  signUpStart: userCredentials => dispatch(signUpStart(userCredentials))
+});
+
+export default connect(null, mapDispatchToProps)(SignUp);
