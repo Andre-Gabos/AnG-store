020bf73f0a95a35a2ad2970bdfc792cbf58cfad6
André Gabos <andre.gabos@gmail.com>
2021-05-12 19:17:46 -0300

    Separação do app entre cliente e server, e adição de servidor para lidar com os pagamentos feitos pelo Stripe no backend.

--- client/src/Components/StripeButton/StripeButton.jsx
+++ client/src/Components/StripeButton/StripeButton.jsx
@@ -1,13 +1,25 @@
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
 
 function StripeCheckoutButton({ price }) {
   const priceForStripe = price * 100;
   const publishableKey = "pk_test_51IhSVgB54jOSmuZ7WgJXTIbAXAYTrdlfS1czeeQAsWjD9ci5BW70q8UXOQRkOrrvHh3dcxn7zHsF2k85zWtaofkW005gVtLRY6";
 
   const onToken = token => {
-    console.log(token);
-    alert("Payment Successful!")
+    axios({
+      url: "payment",
+      method: "post",
+      data: {
+        amount: priceForStripe,
+        token
+      }
+    }).then(response => {
+      alert("Payment was successful");
+    }).catch(error => {
+      console.log("Payment error: ", JSON.parse(error));
+      alert("There was an issue with your payment. Please make sure you use the provided credit card.");
+    })
   }
 
   return (
@@ -16,7 +28,7 @@ function StripeCheckoutButton({ price }) {
       name="AnG Store"
       billingAddress
       shippingAddress
-      image="https://sendeyo.com/en/f3eb2117da"
+      //image
       description={`Your total is $${price}`}
       amount={priceForStripe}
       panelLabel="Pay Now"
